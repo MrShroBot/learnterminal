21757a3d9f0f2e43bf9eb5c88e3d754b57f9e18c
Kaspar Kesküla <Kaspar.Keskyla2@tptlive.ee>
2022-11-08 12:39:15 +0200

    add boxed and chalk

--- run.js
+++ run.js
@@ -1,55 +1,31 @@
 function write(string){
     process.stdout.write(string);
 }
-// //bold                           //reset
-// write("\x1B[1m"); write("Bruh"); write("\x1B[0m"); write("\n");
-// //dim/faint                      //reset
-// write("\x1B[2m"); write("Bruh"); write("\x1B[0m"); write("\n");
-// //italic                         //reset
-// write("\x1B[3m"); write("Bruh"); write("\x1B[0m"); write("\n");
-// //underline                      //reset
-// write("\x1B[4m"); write("Bruh"); write("\x1B[0m"); write("\n");
-// //blincing                       //reset
-// write("\x1B[5m"); write("Bruh"); write("\x1B[0m"); write("\n");
+import chalk from 'chalk';
+import boxen from 'boxen';
 
-// //inverse                        //reset
-// write("\x1B[7m"); write("Bruh"); write("\x1B[0m"); write("\n");
-// //hidden/invisab                 //reset
-// write("\x1B[8m"); write("Bruh"); write("\x1B[0m"); write("\n");
-// //stricethrough                  //reset
-// write("\x1B[9m"); write("Bruh"); write("\x1B[0m"); write("\n");
+write(chalk.blue.bgGreen('Hello chalk'));
 
-// lines 22-39 no worke
 
-// for(let i=30; i<40; i++){
-//     if(i==38) continue;
-//         // color                                          //reset
-//         write("\x1B[1;"+i+"m"); write("color code number"+i); write("\x1B[0m"); write("\n");
-// }
-// for(let i=30; i<40; i++){
-//     if(i==38) continue;
-//         // color                                          //reset
-//         write("\x1B[1;"+i+"m"); write("color code number"+i); write("\x1B[0m"); write("\n");
-// }
-// for(let i=0; i<16; i++){
-//     for(let j=0; j<16; j++){
-//             let color = i*j
-//             // color                                          //reset
-//             write("\x1B[2;"+i+"m"); write("color code number"+i); write("\x1B[0m"); write("\n");
-//     }
-// }
+for(let i=0; i<16; i++){
+    for(let j=0; j<16; j++){
+        let color = (i*j).toString()
+        write(chalk.ansi256(color)(color.padEnd(4,' '))); 
+    }
+    write('\n');
+}
 
-// for(let i=40; i<50; i++){
-//     if(i==48) continue;
-//         // color                                          //reset
-//         write("\x1B[1;"+i+"m"); write("color code number"+i); write("\x1B[0m"); write("\n");
-// }
+write(boxen(chalk.blue.bgGreen('Hello chalk'), {
+    padding: 1,
+    margin:1
+}));
 
-console.clear();
-let date = new Date().toString();
-write(date);
-setInterval(()=>{
-    write("\x1B["+ date.length + "D");
-    date = new Date().toString();
-    write(date)
-}, 1);
+write(boxen(chalk.magenta.bgGreen('Hello boxen'), {
+    padding: 1,
+    margin:1
+}));
+
+write(boxen(chalk.red.bgGreen('Bruh'), {
+    padding: 1,
+    margin:1
+}));
